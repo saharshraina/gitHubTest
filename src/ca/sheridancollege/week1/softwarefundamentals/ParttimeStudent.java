6768abf6eb919a86e1c7c598c4c185b8c3a188cb
sahar <sahar@10.16.40.142>
2019-05-22 15:04:43 -0400

    commit 8

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/ParttimeStudent.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author sahar
+ */
+public class ParttimeStudent {
+    
+    public class ParttimeStudent extends Student{ 
+        private int noofhrs;
+        
+        public ParttimeStudents(String name , String stdio , int hours){
+            super(name ,stdid);
+            this.noofhours;
+    }
+        
+        
+        
+        public int getNoofHours() {
+            return noofHours;
+        }
+        public void setNoOfHours(int noOfHours){
+            this.noOfHours = noOfHours;
+        }
+    }
+}
